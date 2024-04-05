6957c2d2f6988246ac4d477103fdf36d0a8c52aa
Kisaragi Lab <marineblue4894@gmail.com>
2024-04-05 04:25:56 +0100

    XML Configuration for bean setup demonstration

---
+++ src/main/java/com/example/demo/springboot/xmlConfiguration/XmlConfigurationLauncher.java
@@ -0,0 +1,20 @@
+package com.example.demo.springboot.xmlConfiguration;
+
+import java.util.Arrays;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.example.demo.springboot.gameLauncher.game.GameRunner;
+
+public class XmlConfigurationLauncher {
+    
+    public static void main(String[] args) {
+        try(ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println(context.getBean("name"));
+            System.out.println(context.getBean("age"));
+            context.getBean(GameRunner.class).run();
+        }
+	}
+
+}
