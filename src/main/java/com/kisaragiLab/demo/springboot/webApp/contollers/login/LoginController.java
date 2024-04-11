23de23df7e249ad6471a7192fbc6b1bc2fc21ed3
Kisaragi Lab <marineblue4894@gmail.com>
2024-04-11 16:09:03 +0100

    Typical logging practice demonstrated

--- src/main/java/com/kisaragiLab/demo/springboot/webApp/contollers/login/LoginController.java
+++ src/main/java/com/kisaragiLab/demo/springboot/webApp/contollers/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.kisaragiLab.demo.springboot.webApp.contollers.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
 
     private static String PREFIX = "/login";
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/login")
     public String login(@RequestParam String username, @RequestParam String password, ModelMap model) {
+        logger.debug("username: {}", username);
         model.put("username", username);
         model.put("password", password);
         return PREFIX + "/Login";
