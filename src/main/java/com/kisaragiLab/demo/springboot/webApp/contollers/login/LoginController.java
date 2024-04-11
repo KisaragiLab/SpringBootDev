9c43bdf7350abd391103188b5beeb01911f857d4
Kisaragi Lab <marineblue4894@gmail.com>
2024-04-11 15:54:23 +0100

    ModelMap for requested parameters added

--- src/main/java/com/kisaragiLab/demo/springboot/webApp/contollers/login/LoginController.java
+++ src/main/java/com/kisaragiLab/demo/springboot/webApp/contollers/login/LoginController.java
@@ -1,7 +1,9 @@
 package com.kisaragiLab.demo.springboot.webApp.contollers.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -9,7 +11,9 @@ public class LoginController {
     private static String PREFIX = "/login";
 
     @RequestMapping("/login")
-    public String login() {
+    public String login(@RequestParam String username, @RequestParam String password, ModelMap model) {
+        model.put("username", username);
+        model.put("password", password);
         return PREFIX + "/Login";
     }
 
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login/Login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login/Login.jsp
@@ -7,10 +7,9 @@
         <h1>Login to your account</h1>
         <form>
             <label for="usernameLabel">Username:</label>
-            <input type="text" id="username" name="username">
-            <br>
-            <label for="paawordLabel">Password:</label>
-            <input type="text" id="password" name="password"><br><br>
+            <input type="text" id="username" name="username" value=${username}><br>
+            <label for="passwordLabel">Password:</label>
+            <input type="text" id="password" name="password" value=${password}><br>
             <input type="submit" value="Submit">
         </form>
     </body>
