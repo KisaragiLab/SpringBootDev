c5d07bfdd6bd65ef8f5917f55f8c7668809e3a5b
Kisaragi Lab <marineblue4894@gmail.com>
2024-04-17 19:00:01 +0100

    Database connection

--- src/main/java/com/kisaragiLab/demo/springboot/webApp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/kisaragiLab/demo/springboot/webApp/security/SpringSecurityConfiguration.java
@@ -4,11 +4,14 @@ import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -34,6 +37,17 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(req -> req.anyRequest().authenticated());
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+        
+        return httpSecurity.build();
     }
 
 }
